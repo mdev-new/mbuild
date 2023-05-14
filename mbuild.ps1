53f424c94acfe604486c0e24e8a6f466e2c7ad7d
mdev-new <121290257+mdev-new@users.noreply.github.com>
2023-05-14 14:48:10 +0200

    Fix some stuff

--- mbuild.ps1
+++ mbuild.ps1
@@ -2,6 +2,12 @@
 # All rights reserved.
 
 # TODO: Make dependency support
+# TODO: Multithreading.
+
+#todo log file with hashes of command lines
+
+# Tested & (mostly) works on PS 5.1
+# PS7 for some reason rebuilds everytime everything
 
 param (
 	[switch]$help = $false,
@@ -9,11 +15,10 @@ param (
 	[switch]$quiet = $false,
 	[string]$Cdir = ((Get-Item .).FullName) -join '',
 	[string]$file = ".\bcfg.ps1",
-	[string]$target = ""
+	[string]$target = "build"
 )
 
-Set-PSDebug -Trace 0
-pushd $Cdir
+Push-Location $Cdir
 
 if ($help) {
 	Write-Output "MousieBuild v0.3"
@@ -30,76 +35,97 @@ if ($help) {
 # Command output only on verbose mode
 $printVerbose = if ($verbose) {'Out-Default'} else {'Out-Null'}
 $printNormal = if ($quiet -eq $false) {'Out-Default'} else {'Out-Null'}
+$printOnlyNormal = if (($quiet -eq $false) -and -not $verbose) {'Out-Default'} else {'Out-Null'}
 $total = 0
 $done = 1
 
-New-Item -ItemType Directory -Force -Path "tmp" | & $printVerbose
+Set-Alias -Name "Exec" -Value "Invoke-Expression"
 
-$build_targets = New-Object 'System.Collections.Generic.List[string]'
-
-function RecursiveTargetWalk($target) {
+# this function took probably longest to write, idk why
+function FindDeps($targets,$target) {
 	$targets[$target][1] | ForEach-Object {
 		if ($targets.keys -contains "$_") {
-			$build_targets.Insert(0,$_)
-			RecursiveTargetWalk $_
-		} else { Return }
+			Return $_ + (FindDeps $targets $_)
+		} else {
+			Return $null
+		}
 	}
 }
 
 # returns: false if $base < $fnames
-function IsUpToDate([string]$base, [string[]]$fnames){
-	$BaseWriteTime = (Get-Item "$base" -ErrorAction SilentlyContinue).LastWriteTime
+function IsUpToDate([string]$base, [string[]]$fnames) {
 
-	$Results = @();
-
-	$fnames | %{
-		#Write-Host $BaseWriteTime (Get-Item "$_").LastWriteTime
-		$Results += ($BaseWriteTime -ge ((Get-Item "$_").LastWriteTime))
+	# If the product doesn't exist, we'll definitely need a recompile.
+	if([System.IO.File]::Exists($base) -eq $false) {
+		return $false
 	}
 
-	# AND all the results together
+	# This is guaranteed to have a value
+	$BaseWriteTime = (Get-Item "$base").LastWriteTime
+
 	$UpToDate = $true
-	$Results | % {
-		$UpToDate = $UpToDate -and $_
+	$fnames | %{
+		if([System.IO.File]::Exists("$_") -eq $false) {
+			$UpToDate = $false
+		} else {
+			$UpToDate = $UpToDate -and ($BaseWriteTime -gt ((Get-Item "$_").LastWriteTime))
+		}
 	}
 
 	return $UpToDate
 }
 
 function PrintStatus($text) {
-	if (($quiet -eq $false) -or $verbose) { Write-Output ("$prefix$text" -f $done,$total,[int](($done/$total) * 100) ) }
+	if (($quiet -eq $false) -or $verbose) {
+		Write-Output ("$prefix$text" -f $done, $total, [int](($done/$total) * 100))
+	}
 }
 
 function Build($targets) {
-	$build_targets.Add($target)
-
-	RecursiveTargetWalk $target
-	$total = $build_targets.Count
-
-	$build_targets | %{
-		if ($targets[$_][0] -eq '' -or $targets[$_][2] -eq $true) { $total--; }
+	if($target -eq '') {
+		Write-Output "No target specified."
+		Exit
 	}
 
+	$build_targets = @()
+
+	#if phony
+	if($targets[$target][0] -eq '') {
+		$targets[$target][1] | %{
+			$deps = FindDeps $targets $_
+			if($deps -ne $null) { $build_targets += $deps }
+			$build_targets += "$_"
+		}
+	} else {
+		$build_targets += "$target"
+	}
+
+	$total = $build_targets.Count
+
+	$bIsUpToDate = $false
 	$build_targets | %{
-		if ($targets[$_][2] -ne $true -and (IsUpToDate $_ $targets[$_][1] ) -eq $false) {
-			& $targets[$_][0] $targets[$_][1] $_
+		$bIsUpToDate = IsUpToDate $_ $targets[$_][1]
+		if (($targets[$_][0] -ne '') -and ($bIsUpToDate -eq $false)) {
+			$flags = @($targets[$_][2], '')[!($targets[$_].Count -eq 3)]
+			$inpts = ($targets[$_][1] -join " ")
+			& $targets[$_][0] "$inpts" "$_" $flags
 			if ($lastexitcode -ne 0) {
 				Write-Output "BUILD FAILED"
-				Write-Output "Re-run the build with the -v option" | & $printNormal
-				
-				if([System.IO.File]::Exists($_)) { del $_ }
+				Write-Output "Re-run the build with the -v option" | & $printOnlyNormal
+
+				if([System.IO.File]::Exists($_)) { Remove-Item $_ }
 				Exit
 			}
-		} elseif ($targets[$_][2] -ne $true) {
+		} elseif (($targets[$_][0] -eq '') -or ($bIsUpToDate -eq $true)) {
 			PrintStatus "ACTUAL`t$_"
-		} elseif ($targets[$_][0] -ne '') {
-			& $targets[$_][0] $targets[$_][1] $_
 		}
 
 		$done++
 	}
 }
 
+$in_mbuild = $true
+
 # Exec the build file.
 . $file
-popd
+Pop-Location
